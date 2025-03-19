214dd47fe6c94d26faf8c5f3269913747a9f9e50
Harshit Bareja <hbareja.07@gmail.com>
2025-03-19 23:25:50 +0530

    mobile responsive

--- app/components/Weather.tsx
+++ app/components/Weather.tsx
@@ -107,70 +107,90 @@ const Weather: React.FC = () => {
   };
 
   return (
-    <div className="w-screen h-screen flex bg-cover bg-center max-h-screen max-w-screen overflow-hidden max-md:flex-col max-md:h-200vh max-md:max-h-200vh"
-    style={getBackgroundStyle()}>
-      <div className="h-full w-[65%] flex justify-start items-end max-md:w-[100%] max-md:opacity-[50%] max-md:justify-center max-md:items-center">
-      <div className="flex flex-row w-[50%] h-[15%] justify-end max-md:h-[50%] max-md:justify-center">
-        <div className="w-[45%] h-full text-8xl flex justify-end items-center">{weatherData?.temperature}°C</div>
-        <div className="w-[45%] h-full flex flex-col justify-center items-start text-lg ml-10px">
-          <span className=" h-[25%]">{weatherData?.city}</span>
-          <span className=" h-[25%]">{weatherData?.day_of_week}</span>
-          <div className="flex justify-center items-center h-[25%]"><span>{weatherData?.weather}</span><img className="h-full"src={weatherData?.icon}/></div>
+    <div
+      className="w-screen h-screen flex bg-cover bg-center max-h-screen max-w-screen overflow-hidden
+                 max-md:flex-col max-md:h-[150vh] max-md:max-h-[150vh] max-md:overflow-hidden"
+      style={getBackgroundStyle()}
+    >
+      {/* First Div (Weather Display) */}
+      <div
+        className="h-full w-[65%] flex justify-start items-end 
+                   max-md:w-[100%] max-md:h-[50vh] max-md:justify-center max-md:items-center max-md:opacity-[30%] max-md:bg-black max-md:text-white max-md:text-opacity-100"
+      >
+        <div className="flex flex-row w-[50%] h-[15%] justify-end max-md:h-[80%] max-md:w-[100%] ">
+          <div className="w-[45%] h-full text-8xl flex justify-end items-center max-md:justify-end max-md:w-[50%]">
+            {weatherData?.temperature}°C
+          </div>
+          <div className="w-[45%] h-full flex flex-col justify-center items-start text-lg ml-10px max-md:text-md max-md:w-[50%] max-md:h-[100%] max-md:justify-center">
+            <span className="h-[25%]">{weatherData?.city}</span>
+            <span className="h-[25%]">{weatherData?.day_of_week}</span>
+            <div className="flex justify-center items-center h-[25%]">
+              <span>{weatherData?.weather}</span>
+              <img className="h-full" src={weatherData?.icon} />
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-      <div className="h-full w-[35%] bg-black opacity-[50%] text-white flex flex-col justify-center items-center gap-2 p-4 max-md:w-[100%]">
+  
+      {/* Second Div (Search & Weather Details) */}
+      <div
+        className="h-full w-[35%] bg-black opacity-[50%] text-white flex flex-col justify-center items-center gap-2 p-4
+                   max-md:w-[100%] max-md:h-[100vh] max-md:opacity-[30%] max-md:text-opacity-[100%] max-md:text-white"
+      >
+        {/* Search Input */}
         <div className="flex h-[15%] w-[80%]">
           <form onSubmit={handleSubmit} className="flex w-full gap-2 relative justify-center w-[80%]">
-            <input 
-              className="w-full h-[50%] bg-white rounded-sm text-black p-2 box-border outline-none" 
-              type="text" 
-              placeholder="Enter city..." 
+            <input
+              className="w-full h-[50%] bg-white rounded-sm text-black p-2 box-border outline-none"
+              type="text"
+              placeholder="Enter city..."
               onChange={handleInput}
             />
-            <button type="submit" className=" absolute right-[2%] top-[12%] cursor-pointer"><Image src={search} alt="search" width={24} height={24} /></button>
+            <button type="submit" className="absolute right-[2%] top-[12%] cursor-pointer">
+              <Image src={search} alt="search" width={24} height={24} />
+            </button>
           </form>
         </div>
-        
-            {/* Weather Details */}
-            <div className="flex flex-col h-[25%] w-[80%] border-b-1 justify-evenly box-border pb-[5%]">
-              <span className="text-xl mb-5">Weather Details</span>
-              <div className="flex justify-between w-full">
-                <span>Humidity:</span><span>{weatherData?.humidity}%</span>
-              </div>
-              <div className="flex justify-between w-full">
-                <span>Wind Speed:</span><span>{weatherData?.wind_speed} km/h</span>
-              </div>
-              <div className="flex justify-between w-full">
-                <span>Wind Dir:</span><span>{weatherData?.wind_dir}</span>
-              </div>
-              <div className="flex justify-between w-full">
-                <span>Condition:</span><span>{weatherData?.weather}</span>
-              </div>
-            </div>
-
-            {/* Air Quality & Additional Data */}
-            <div className="flex flex-col h-[30%] w-[80%] mt-[5%]">
-              <span className="text-xl mb-5">Additional Info</span>
-              <div className="grid grid-cols-2 gap-5 text-center w-full">
-                {/* First Row (Two Columns) */}
-                <span className="col-span-1 flex justify-start">Pressure: {weatherData?.pressure_mb} mb</span>
-                <span className="col-span-1 flex justify-end">Feels Like: {weatherData?.feels_like} °C</span>
-
-                {/* Second Row (One Column - Centered) */}
-                <span className="col-span-2 flex justify-center">
-                  Visibility: {weatherData?.visibility_km} km
-                </span>
-              </div>
-            </div>
-        
-
+  
+        {/* Weather Details */}
+        <div className="flex flex-col h-[25%] w-[80%] border-b-1 justify-evenly box-border pb-[5%] max-md:h-[30%]">
+          <span className="text-xl mb-5">Weather Details</span>
+          <div className="flex justify-between w-full">
+            <span>Humidity:</span>
+            <span>{weatherData?.humidity}%</span>
+          </div>
+          <div className="flex justify-between w-full">
+            <span>Wind Speed:</span>
+            <span>{weatherData?.wind_speed} km/h</span>
+          </div>
+          <div className="flex justify-between w-full">
+            <span>Wind Dir:</span>
+            <span>{weatherData?.wind_dir}</span>
+          </div>
+          <div className="flex justify-between w-full">
+            <span>Condition:</span>
+            <span>{weatherData?.weather}</span>
+          </div>
+        </div>
+  
+        {/* Additional Info */}
+        <div className="flex flex-col h-[30%] w-[80%] mt-[5%] max-md:mt-[2%] max-md:h-[25%]">
+          <span className="text-xl mb-5">Additional Info</span>
+          <div className="grid grid-cols-2 gap-5 text-center w-full">
+            <span className="col-span-1 flex justify-start">Pressure: {weatherData?.pressure_mb} mb</span>
+            <span className="col-span-1 flex justify-end">Feels Like: {weatherData?.feels_like} °C</span>
+            <span className="col-span-2 flex justify-center">Visibility: {weatherData?.visibility_km} km</span>
+          </div>
+        </div>
+  
+        {/* Forecast Section */}
         <div className="flex h-[10%] w-[90%]">
           <span className="w-full text-right text-sm">Weather Forecast</span>
         </div>
       </div>
     </div>
   );
+  
 };
 
 export default Weather;
